e6c246a5ba92dcf396ed338d595d8435467305b9
Harvey Yerik <yerik05vh@gmail.com>
2023-01-19 14:14:35 -0500

    structure destination done

--- src/components/screen/DestinationComponent.tsx
+++ src/components/screen/DestinationComponent.tsx
@@ -1,12 +1,34 @@
 import React from "react";
+/* components */
+import NavegatorComponent from "../NavegatorComponent";
+/* images */
+import mars from "../../assets/destination/image-mars.png";
+import europa from "../../assets/destination/image-europa.png";
+import moon from "../../assets/destination/image-moon.png";
+import titan from "../../assets/destination/image-titan.png";
+/* styles */
 import {
   BgGround,
+  BStyle,
+  Decoration,
+  DivBoxOne,
+  DivBoxTwo,
   DivGridOne,
   DivGridTwo,
+  DivStart,
+  DivTitle,
+  HeadlinePrincipal,
+  HeadlineSecond,
+  ImagePlanet,
+  ImageRespon,
   MainStyle,
+  Navigation,
+  NavigationOptions,
   SectionStyle,
+  Subtitle,
+  TextBodyOne,
+  TextBodyTwo,
 } from "../../styles/destination_style";
-import NavegatorComponent from "../NavegatorComponent";
 
 export default function DestinationComponent() {
   return (
@@ -15,8 +37,43 @@ export default function DestinationComponent() {
       <NavegatorComponent />
       <MainStyle>
         <SectionStyle>
-          <DivGridOne></DivGridOne>
-          <DivGridTwo></DivGridTwo>
+          <DivGridOne>
+            <DivStart>
+              <DivTitle>
+                <BStyle>01</BStyle>
+                <HeadlineSecond>Pick your destination</HeadlineSecond>
+              </DivTitle>
+            </DivStart>
+            <ImageRespon>
+              <ImagePlanet src={moon} />
+            </ImageRespon>
+          </DivGridOne>
+          <DivGridTwo>
+            <Navigation>
+              <NavigationOptions>moon</NavigationOptions>
+              <NavigationOptions>mars</NavigationOptions>
+              <NavigationOptions>europa</NavigationOptions>
+              <NavigationOptions>titan</NavigationOptions>
+            </Navigation>
+            <HeadlinePrincipal>moon</HeadlinePrincipal>
+            <TextBodyOne>
+              See our planet as you’ve never seen it before. A perfect relaxing
+              trip away to help regain perspective and come back refreshed.
+              While you’re there, take in some history by visiting the Luna 2
+              and Apollo 11 landing sites.
+            </TextBodyOne>
+            <Decoration></Decoration>
+            <DivBoxOne>
+              <DivBoxTwo>
+                <Subtitle>AVG. DISTANCE</Subtitle>
+                <TextBodyTwo>384,400 km</TextBodyTwo>
+              </DivBoxTwo>
+              <DivBoxTwo>
+                <Subtitle>Est. travel time</Subtitle>
+                <TextBodyTwo>3 days</TextBodyTwo>
+              </DivBoxTwo>
+            </DivBoxOne>
+          </DivGridTwo>
         </SectionStyle>
       </MainStyle>
     </>
--- src/styles/destination_style.ts
+++ src/styles/destination_style.ts
@@ -26,12 +26,56 @@ export const MainStyle = styled.main`
   margin: 0 auto;
   position: relative;
   width: 100%;
+  @media (min-width: 720px) {
+    margin-top: 2.5rem;
+  }
+  @media (min-width: 720px) {
+    margin-top: 4.75rem;
+  }
 `;
-export const SectionStyle = styled.section``;
-export const DivGridOne = styled.div``;
-export const DivGridTwo = styled.div``;
-
-export const DivTitle = styled.div``;
+export const SectionStyle = styled.section`
+  display: grid;
+  place-items: center;
+  @media (min-width: 1000px) {
+    grid-template-columns: 50% 50%;
+  }
+`;
+export const DivGridOne = styled.div`
+  width: 100%;
+  display: grid;
+  place-items: center;
+`;
+export const DivGridTwo = styled.div`
+  display: grid;
+  place-items: center;
+  width: 100%;
+  margin-top: 1.625rem;
+  padding: 0 1.625rem;
+  margin-bottom: 3.625rem;
+  @media (min-width: 720px) {
+    margin-top: 3.313rem;
+  }
+  @media (min-width: 1000px) {
+    place-items: start;
+  }
+`;
+export const DivStart = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 0 2.375rem;
+  @media (min-width: 720px) {
+    justify-content: start;
+  }
+`;
+export const DivTitle = styled.div`
+  display: flex;
+  gap: 1.125rem;
+  @media (min-width: 1000px) {
+    gap: 1.75rem;
+  }
+`;
 export const BStyle = styled.b`
   text-align: center;
   letter-spacing: 2.7px;
@@ -73,14 +117,16 @@ export const HeadlineSecond = styled.h2`
 export const ImageRespon = styled.div`
   width: 10.625rem;
   height: 10.625rem;
-
+  margin-top: 2rem;
   @media (min-width: 720px) {
     width: 18.75rem;
     height: 18.75rem;
+    margin-top: 3.75rem;
   }
   @media (min-width: 1000px) {
     width: 27.813rem;
     height: 27.813rem;
+    margin-top: 4.188rem;
   }
 `;
 export const ImagePlanet = styled.img`
@@ -93,28 +139,34 @@ export const TextBodyOne = styled.p`
   font: normal normal 400 0.938rem/1.563rem var(--font-barlow);
   text-align: center;
   width: 100%;
+  margin-top: 0.75rem;
   @media (min-width: 720px) {
     font-size: 1rem;
     line-height: 1.75rem;
-    max-width: 27.75rem;
+    max-width: 35.813rem;
+    margin-top: 0.5rem;
   }
   @media (min-width: 1000px) {
     font-size: 1.125rem;
     line-height: 2rem;
     text-align: left;
-    max-width: 35.813rem;
+    max-width: 27.75rem;
   }
 `;
 
 export const Navigation = styled.nav`
   display: flex;
-  gap: 1rem;
+  gap: 1.625rem;
+  @media (min-width: 720px) {
+    gap: 2.25;
+  }
 `;
 export const NavigationOptions = styled.button`
   letter-spacing: 2.3625px;
   font: normal normal 400 0.875rem/1.063rem var(--font-barlow-condensed);
   color: var(--white);
   background-color: transparent;
+  text-transform: uppercase;
   @media (min-width: 720px) {
     font-size: 1rem;
     line-height: 1.188rem;
@@ -126,31 +178,63 @@ export const HeadlinePrincipal = styled.h1`
   color: var(--white);
   font: normal normal 400 3.5rem/4rem var(--font-bellefair);
   text-transform: uppercase;
+  margin-top: 1.25rem;
   @media (min-width: 720px) {
-    font-size: 9.375rem;
-    line-height: 9.375rem;
+    font-size: 5rem;
+    line-height: 5.75rem;
+    margin-top: 2rem;
   }
   @media (min-width: 1000px) {
     font-size: 9.375rem;
     line-height: 10.75rem;
   }
 `;
-export const Decoration = styled.div``;
-export const DivBoxOne = styled.div``;
+export const Decoration = styled.div`
+  margin: 2rem 0;
+  height: 1px;
+  width: 100%;
+  background-color: var(--black);
+  max-width: 35.813rem; ;
+`;
+export const DivBoxOne = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  justify-content: center;
+  @media (min-width: 720px) {
+    flex-direction: row;
+    width: 100%;
+    gap: 6rem;
+  }
+  @media (min-width: 1000px) {
+    justify-content: start;
+  }
+`;
 export const DivBoxTwo = styled.div``;
 export const Subtitle = styled.h3`
   text-align: center;
   letter-spacing: 2.3625px;
   text-transform: uppercase;
-  font: normal normal 400 0.875rem/1.063rem (--font-barlow-condensed);
-
+  font: normal normal 400 0.875rem/1.063rem var(--font-barlow-condensed);
   color: var(--purple-light);
+  @media (min-width: 720px) {
+    text-align: left;
+  }
+  @media (min-width: 1000px) {
+    text-align: left;
+  }
 `;
 
 export const TextBodyTwo = styled.p`
   font: normal normal 400 1.75rem/2rem var(--font-bellefair);
   text-align: center;
   text-transform: uppercase;
-
   color: var(--white);
+  @media (min-width: 720px) {
+    margin-top: 0.75rem;
+    text-align: left;
+  }
+  @media (min-width: 1000px) {
+    text-align: left;
+  }
 `;
