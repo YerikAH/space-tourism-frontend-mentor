7a670301d44790c41492a8d842c6b97201d99e80
Harvey Yerik <yerik05vh@gmail.com>
2023-01-26 19:33:02 -0500

    context success :)

--- src/components/screen/TechComponent.tsx
+++ src/components/screen/TechComponent.tsx
@@ -1,7 +1,10 @@
 import React, { useContext } from "react";
-import fetchContext from "../../context/fetchContext";
+import FetchContext from "../../context/fetchContext";
 export default function TechComponent() {
-  const data = useContext(fetchContext);
-  // console.log(data);
-  return <div className="">{data.home[0].button}</div>;
+  const data = useContext(FetchContext);
+  return (
+    <FetchContext.Provider value={data}>
+      <div className="">{data.home[0].button}</div>
+    </FetchContext.Provider>
+  );
 }
