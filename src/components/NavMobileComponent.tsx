120e07ee602f65d1c79c7f74cbff6fdc0417b111
Harvey Yerik <yerik05vh@gmail.com>
2023-01-30 22:55:23 -0500

    Dev: add nav mobile

--- src/components/NavMobileComponent.tsx
+++ src/components/NavMobileComponent.tsx
@@ -1,5 +1,51 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import { Link } from "react-router-dom";
+import {
+  BStyle,
+  List,
+  MenuDivOne,
+  MenuDivTwo,
+  MenuList,
+  MenuMobile,
+  SpanStyle,
+} from "../styles/navegator_mobile_style";
 
 export default function NavMobileComponent() {
-  return <div>NavMobileComponent</div>;
+  const elementHTML = document.getElementById("menu")!;
+  return ReactDOM.createPortal(
+    <MenuMobile>
+      <MenuList>
+        <MenuDivOne></MenuDivOne>
+        <MenuDivTwo>
+          <List>
+            <Link to="/" className="Link">
+              <BStyle>00</BStyle>
+              <SpanStyle>HOME</SpanStyle>
+            </Link>
+          </List>
+          <List>
+            <Link to="/destination" className="Link">
+              <BStyle>01</BStyle>
+              <SpanStyle>DESTINATION</SpanStyle>
+            </Link>
+          </List>
+          <List>
+            <Link to="/crew" className="Link">
+              <BStyle>02</BStyle>
+              <SpanStyle>CREW</SpanStyle>
+            </Link>
+          </List>
+          <List>
+            {" "}
+            <Link to="/technology" className="Link">
+              <BStyle>03</BStyle>
+              <SpanStyle>TECHNOLOGY</SpanStyle>
+            </Link>
+          </List>
+        </MenuDivTwo>
+      </MenuList>
+    </MenuMobile>,
+    elementHTML
+  );
 }
--- src/styles/navegator_mobile_style.ts
+++ src/styles/navegator_mobile_style.ts
@@ -1,1 +1,37 @@
 import styled from "styled-components";
+
+export const MenuMobile = styled.nav`
+  display: flex;
+  justify-content: end;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+`;
+export const MenuList = styled.ul`
+  height: inherit;
+  width: 15.875rem;
+  display: grid;
+  background: var(--white-transparent);
+  backdrop-filter: blur(40.7742px);
+`;
+export const MenuDivOne = styled.div``;
+export const MenuDivTwo = styled.div`
+  gap: 2rem;
+`;
+export const List = styled.li`
+  .Link {
+    gap: 11px;
+  }
+`;
+export const BStyle = styled.b`
+  font: normal normal 700 1rem/1.25rem var(--font-barlow-condensed);
+  letter-spacing: 2.7px;
+  color: var(--white);
+`;
+export const SpanStyle = styled.span`
+  font: normal normal 400 1rem/1.25rem var(--font-barlow-condensed);
+  letter-spacing: 2.7px;
+  color: var(--white);
+`;
